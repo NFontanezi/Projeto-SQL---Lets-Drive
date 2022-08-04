22d34ace4a11b7f6efecd8a1da0c790244efd80a
Natália Campos <nataliacamposnccs@gmail.com>
2022-08-04 19:17:58 -0300

    Queries Pedido

--- Queries e procedures/Query Pedido.sql
+++ Queries e procedures/Query Pedido.sql
@@ -1,28 +1,28 @@
 
 -- NATÁLIA CAMPOS
 -- Select dos carros alugados numa data X
-SELECT DISTINCT C.Cod_carro, C.Modelo, C.Placa FROM carro AS C
+SELECT DISTINCT C.placa, C.modelo FROM carro AS C
 JOIN pedido AS P
-ON C.Cod_carro = P.cod_carro
+ON C.placa = P.placa
 WHERE '2022/02/01' BETWEEN P.data_aluguel AND P.data_retorno;
 
 
 -- NATÁLIA CAMPOS
 -- Select dos carro que não estão alugados na mesma data X
-SELECT C.Cod_carro, C.Modelo, C.Placa FROM carro AS C
+SELECT C.placa, C.modelo FROM carro AS C
 JOIN pedido AS P
-ON C.Cod_carro = P.cod_carro
+ON C.placa = P.placa
 WHERE '2022/02/01' NOT BETWEEN P.data_aluguel AND P.data_retorno
 EXCEPT 
-SELECT C.Cod_carro, C.Modelo, C.Placa FROM carro AS C
+SELECT C.placa, C.modelo FROM carro AS C
 JOIN pedido AS P
-ON C.Cod_carro = P.cod_carro
+ON C.placa = P.placa
 WHERE '2022/02/01' BETWEEN P.data_aluguel AND P.data_retorno;
 
 
 -- NATÁLIA CAMPOS
 -- Fazer uma função de calcular valor total pedido
-CREATE FUNCTION CalculaDiaria(@Placa VARCHAR, @Diarias INT)
+CREATE FUNCTION CalculaDiaria(@Placa VARCHAR(7), @Diarias INT)
 RETURNS money
 BEGIN
    RETURN (SELECT (preco_diaria * @Diarias) FROM carro WHERE placa = @Placa)
